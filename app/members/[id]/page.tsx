d38325e5f6854229000e93ac113215ac4f7abc5e
adam <admmft14@gmail.com>
2025-06-07 16:14:51 +0800

    fix: edit members

--- app/members/[id]/page.tsx
+++ app/members/[id]/page.tsx
@@ -76,7 +76,7 @@ async function MemberDetails({ memberId }: { memberId: number }) {
             </Link>
           </Button>
           <Button variant="outline" asChild>
-            <Link href={`/members/${member.id}/edit`}>
+            <Link href={`/members/${member.id}/edit`}  onClick={(e) => e.stopPropagation()}>
               <EditIcon className="mr-2 h-4 w-4" />
               Edit Member
             </Link>
